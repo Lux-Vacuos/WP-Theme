447a8f891c84460e77e1c0e549a08ed3f910d572
Guerra24 <pablo230699@hotmail.com>
2016-08-29 12:16:23 -0600

    Fixed li dot

--- sidebar.php
+++ sidebar.php
@@ -1,10 +1,10 @@
 <div class="right col-md-3">
 	<!--  
 	<h4 class="min_title">Categories</h4>
-	<ul>
-		<li><a href="#">Lux Vacuos</a></li>
-		<li><a href="#">Voxel</a></li>
-		<li><a href="#">Infinity</a></li>
+	<ul class="categories">
+		<li class="cat"><a href="#">Lux Vacuos</a></li>
+		<li class="cat"><a href="#">Voxel</a></li>
+		<li class="cat"><a href="#">Infinity</a></li>
 	</ul>
 	-->
 	<h4 class="min_title">Archives</h4>
@@ -16,10 +16,10 @@
 	<p><?php the_author_meta( 'description' ); ?> </p>
 	 -->
 	<h4 class="min_title">Follow US</h4>
-	<a id="youtube" href="<?php echo get_option('youtube'); ?>"> <img
+	<a class="folllink" href="<?php echo get_option('youtube'); ?>"> <img
 		class="follicon"
 		src="<?php bloginfo('template_directory');?>/data/images/yt-icon.png">
-	</a> <a id="twitter" href="<?php echo get_option('twitter'); ?>"> <img
+	</a> <a class="folllink" href="<?php echo get_option('twitter'); ?>"> <img
 		class="follicon"
 		src="<?php bloginfo('template_directory');?>/data/images/tw-icon.png">
 	</a>
