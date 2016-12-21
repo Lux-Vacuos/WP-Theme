92fdd647ad8bca5b4196c07db59b8447d9e3b4b9
Guerra24 <pablo230699@hotmail.com>
2016-12-21 13:48:45 -0600

    Add 404 page

---
+++ 404.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<div class="row">
+
+	<div class="col-md-9">
+
+		<div class="post">
+			<h2 class="blog-post-title">
+				<a href="#">Whoops! Page not found</a>
+			</h2>
+			<p class="blog-post-meta"><?php echo date("F j, Y"); ?> by <a href="#">Lux Vacuos</a>
+			</p>
+			<p>The page you are searching was not found! Something was here
+				before, sorry for that!</p>
+		</div>
+
+	</div>
+	
+	<?php get_sidebar(); ?>
+	
+</div>
+
+<?php get_footer(); ?>
